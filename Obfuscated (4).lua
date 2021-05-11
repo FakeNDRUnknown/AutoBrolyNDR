d9f47982c4acc150efe2ac059e2cf7b6e820f7e1
nabildroiid <81918263+nabildroiid@users.noreply.github.com>
2021-05-11 23:47:54 +0200

    Update Obfuscated (4).lua

--- Obfuscated (4).lua
+++ Obfuscated (4).lua
@@ -1,10 +1,10 @@
-repeat wait(10)
+repeat wait(15)
 game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Update", Text = "enter to the discord group and update the script"})
-  wait()
+  wait(5)
   game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Actualizacion", Text = "Entra al grupo de discord y actualiza el script"})
-  wait()
+  wait(5)
   game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Update", Text = "you need delete the 4 scripts with my autobroly"})
-  wait()
+  wait(5)
   game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Update", Text = "necesitas borrar los 4 scripts de mi autobroly"})
   until FuckOF
 
