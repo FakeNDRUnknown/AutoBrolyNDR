496a38eac0b16cd9d0e6b99e8d6038d9375ba4ac
nabildroiid <81918263+nabildroiid@users.noreply.github.com>
2021-05-11 23:48:22 +0200

    Update Obfuscated (4).lua

--- Obfuscated (4).lua
+++ Obfuscated (4).lua
@@ -1,4 +1,4 @@
-repeat wait(5)
+repeat wait(15)
 game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Update", Text = "enter to the discord group and update the script"})
   wait(5)
   game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Actualizacion", Text = "Entra al grupo de discord y actualiza el script"})
