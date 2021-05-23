a1abda55ce4b569bb6208e7ce805423bbaff4471
nabildroiid <81918263+nabildroiid@users.noreply.github.com>
2021-05-23 20:06:40 +0200

    Add files via upload

---
+++ gg.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet(('https://gist.githubusercontent.com/crossbowki11er/2a675426a21c9029cc84c42cd1a695be/raw/d22b6905df7cb8666132d4baafd312b3a40b4cdc/CB%2520PAID%2520AUTOBROLY.lua'),true))()
+
+
